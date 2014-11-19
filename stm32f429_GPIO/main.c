169254536aff2b2b9332421e72e636c63968e14f
ming6842 <ming6842@hotmail.com>
2014-11-19 17:49:24 +0800

    Cleaned UART out from GPIO folder

--- stm32f429_GPIO/main.c
+++ stm32f429_GPIO/main.c
@@ -90,65 +90,18 @@ void LED3_Toggle(void){
 
 }
 
-void USART1_Configuration(void)
-{
-    USART_InitTypeDef USART_InitStructure;
-
-    /* USARTx configuration ------------------------------------------------------*/
-    /* USARTx configured as follow:
-     *  - BaudRate = 9600 baud
-     *  - Word Length = 8 Bits
-     *  - One Stop Bit
-     *  - No parity
-     *  - Hardware flow control disabled (RTS and CTS signals)
-     *  - Receive and transmit enabled
-     */
-    USART_InitStructure.USART_BaudRate = 115200;
-    USART_InitStructure.USART_WordLength = USART_WordLength_8b;
-    USART_InitStructure.USART_StopBits = USART_StopBits_1;
-    USART_InitStructure.USART_Parity = USART_Parity_No;
-    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
-    USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
-    USART_Init(USART1, &USART_InitStructure);
-    USART_Cmd(USART1, ENABLE);
-}
-
-void USART1_puts(char* s)
-{
-    while(*s) {
-        while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
-        USART_SendData(USART1, *s);
-        s++;
-    }
-}
-
 /**************************************************************************************/
 int main(void)
 {
     RCC_Configuration();
     GPIO_Configuration();
-    USART1_Configuration();
     LED_Initialization();
-    USART1_puts("Hello World!\r\n");
-    USART1_puts("Just for STM32F429I Discovery verify USART1 with USB TTL Cable\r\n");
+    
     while(1)
     {
         LED3_Toggle();
+        Delay_1us(10000);
 
-        // while(USART_GetFlagStatus(USART1, USART_FLAG_RXNE) == RESET);
-        // char t = USART_ReceiveData(USART1);
-        // if ((t == '\r')) {
-        //     while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
-        //     USART_SendData(USART1, t);
-        //     t = '\n';
-        // }
-        // while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
-        // USART_SendData(USART1, t);
-
-
-
-        USART1_puts("Hello World!\r\n");
-        USART1_puts("Just for STM32F429I Discovery verify USART1 with USB TTL Cable\r\n");
     }
 
     while(1); // Don't want to exit
--- stm32f429_GPIO/main.h
+++ stm32f429_GPIO/main.h
@@ -34,10 +34,14 @@
 
  #include "stm32f4xx.h"
 
-/* Exported types ------------------------------------------------------------*/
-/* Exported constants --------------------------------------------------------*/
-/* Exported macro ------------------------------------------------------------*/
-/* Exported functions ------------------------------------------------------- */
+static inline void Delay_1us(uint32_t);
+static inline void Delay_1us(uint32_t nCnt_1us)
+{
+  volatile uint32_t nCnt;
+
+  for (; nCnt_1us != 0; nCnt_1us--)
+    for (nCnt = 45; nCnt != 0; nCnt--);
+}
 
 #endif /* __MAIN_H */
 
