9b7230fd0f79100e8762f1b075ba0b5bb6ed3c7f
ming6842 <ming6842@hotmail.com>
2014-11-20 16:18:58 +0800

    Creating EXTI example

---
+++ stm32f429_EXTI/main.c
@@ -0,0 +1,169 @@
+
+#include "main.h"
+#include <stdio.h>
+
+void RCC_Configuration(void)
+{
+      /* --------------------------- System Clocks Configuration -----------------*/
+      /* USART1 clock enable */
+      RCC_APB2PeriphClockCmd(RCC_APB2Periph_USART1, ENABLE);
+      /* GPIOA clock enable */
+      RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+}
+ 
+/**************************************************************************************/
+ 
+void GPIO_Configuration(void)
+{
+    GPIO_InitTypeDef GPIO_InitStructure;
+
+    /*-------------------------- GPIO Configuration ----------------------------*/
+    GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9 | GPIO_Pin_10;
+    GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AF;
+    GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+    GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL;
+    GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+    GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+    /* Connect USART pins to AF */
+    GPIO_PinAFConfig(GPIOA, GPIO_PinSource9, GPIO_AF_USART1);   // USART1_TX
+    GPIO_PinAFConfig(GPIOA, GPIO_PinSource10, GPIO_AF_USART1);  // USART1_RX
+}
+ 
+/**************************************************************************************/
+
+void ADC_Initialization(void)
+{
+  ADC_InitTypeDef       ADC_InitStructure;
+  ADC_CommonInitTypeDef ADC_CommonInitStructure;
+  GPIO_InitTypeDef      GPIO_InitStructure;
+
+  /* Enable ADC3, DMA2 and GPIO clocks ****************************************/
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOF, ENABLE);
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC3, ENABLE);
+
+  /* Configure ADC3 Channel7 pin as analog input ******************************/
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_9;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AN;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL ;
+  GPIO_Init(GPIOF, &GPIO_InitStructure);
+
+  /* ADC Common Init **********************************************************/
+  ADC_CommonInitStructure.ADC_Mode = ADC_Mode_Independent;
+  ADC_CommonInitStructure.ADC_Prescaler = ADC_Prescaler_Div2;
+  ADC_CommonInitStructure.ADC_DMAAccessMode = ADC_DMAAccessMode_Disabled;
+  ADC_CommonInitStructure.ADC_TwoSamplingDelay = ADC_TwoSamplingDelay_5Cycles;
+  ADC_CommonInit(&ADC_CommonInitStructure);
+
+  /* ADC3 Init ****************************************************************/
+  ADC_InitStructure.ADC_Resolution = ADC_Resolution_12b;
+  ADC_InitStructure.ADC_ScanConvMode = DISABLE;
+  ADC_InitStructure.ADC_ContinuousConvMode = ENABLE;
+  ADC_InitStructure.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None;
+  ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_T1_CC1;
+  ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;
+  ADC_InitStructure.ADC_NbrOfConversion = 1;
+  ADC_Init(ADC3, &ADC_InitStructure);
+
+  /* ADC3 regular channel7 configuration *************************************/
+  ADC_RegularChannelConfig(ADC3, ADC_Channel_7, 1, ADC_SampleTime_3Cycles);
+
+  /* Enable ADC3 */
+  ADC_Cmd(ADC3, ENABLE);
+}
+
+/**************************************************************************************/
+ 
+void LED_Initialization(void){
+
+  GPIO_InitTypeDef  GPIO_InitStructure;
+
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOG , ENABLE); //LED3/4 GPIO Port
+
+  /* Configure the GPIO_LED pin */
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_13 | GPIO_Pin_14;  // LED is connected to PG13/PG14
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_OUT;
+  GPIO_InitStructure.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_UP;
+  GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
+  GPIO_Init(GPIOG, &GPIO_InitStructure);
+
+}
+
+void LED3_Toggle(void){
+
+
+  GPIOG->ODR ^= GPIO_Pin_13;
+
+}
+
+void USART1_Configuration(void)
+{
+    USART_InitTypeDef USART_InitStructure;
+
+    /* USARTx configuration ------------------------------------------------------*/
+    /* USARTx configured as follow:
+     *  - BaudRate = 57600 baud
+     *  - Word Length = 8 Bits
+     *  - One Stop Bit
+     *  - No parity
+     *  - Hardware flow control disabled (RTS and CTS signals)
+     *  - Receive and transmit enabled
+     */
+    USART_InitStructure.USART_BaudRate = 57600;
+    USART_InitStructure.USART_WordLength = USART_WordLength_8b;
+    USART_InitStructure.USART_StopBits = USART_StopBits_1;
+    USART_InitStructure.USART_Parity = USART_Parity_No;
+    USART_InitStructure.USART_HardwareFlowControl = USART_HardwareFlowControl_None;
+    USART_InitStructure.USART_Mode = USART_Mode_Rx | USART_Mode_Tx;
+    USART_Init(USART1, &USART_InitStructure);
+    USART_Cmd(USART1, ENABLE);
+}
+
+void USART1_puts(char* s)
+{
+    while(*s) {
+        while(USART_GetFlagStatus(USART1, USART_FLAG_TXE) == RESET);
+        USART_SendData(USART1, *s);
+        s++;
+    }
+}
+
+/**************************************************************************************/
+
+uint8_t buff_transmit[100];
+int main(void)
+{
+    RCC_Configuration();
+    GPIO_Configuration();
+    USART1_Configuration();
+    LED_Initialization();
+    ADC_Initialization();
+
+    uint16_t adc_data=0;
+    int i=0;
+    USART1_puts("Hello World!\r\n");
+    USART1_puts("Just for STM32F429I Discovery verify USART1 with USB TTL Cable\r\n");
+    
+    ADC_SoftwareStartConv(ADC3);
+
+    while(1)
+    {
+        LED3_Toggle();
+
+        adc_data = ADC_GetConversionValue(ADC3);
+
+        sprintf((char *)buff_transmit, "%d\r\n",(adc_data));
+          USART1_puts((char *)buff_transmit);
+
+          for (i=0;i<50;i++){
+
+            buff_transmit[i]=0;
+          }
+          
+        Delay_1us(100000);
+    }
+
+    while(1); // Don't want to exit
+}
+
---
+++ stm32f429_EXTI/main.h
@@ -0,0 +1,27 @@
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+ #include "stm32f4xx.h"
+
+
+void RCC_Configuration(void);
+void GPIO_Configuration(void);
+void ADC_Initialization(void);
+void LED_Initialization(void);
+void LED3_Toggle(void);
+void USART1_Configuration(void);
+void USART1_puts(char* s);
+static inline void Delay_1us(uint32_t);
+static inline void Delay_1us(uint32_t nCnt_1us)
+{
+  volatile uint32_t nCnt;
+
+  for (; nCnt_1us != 0; nCnt_1us--)
+    for (nCnt = 45; nCnt != 0; nCnt--);
+}
+
+#endif /* __MAIN_H */
+
+
