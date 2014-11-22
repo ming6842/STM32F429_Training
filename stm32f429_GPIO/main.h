db84eb9b13231001375cf7ca6db41ab4bc5179c8
ming6842 <ming6842@hotmail.com>
2014-11-22 18:48:40 +0800

    Add prototype

--- stm32f429_GPIO/main.h
+++ stm32f429_GPIO/main.h
@@ -10,6 +10,9 @@ void RCC_Configuration(void);
 void GPIO_Configuration(void);
 void LED_Initialization(void);
 void LED3_Toggle(void);
+uint8_t PushButton_Read(void);
+void LED3_On(void);
+void LED3_Off(void);
 static inline void Delay_1us(uint32_t);
 static inline void Delay_1us(uint32_t nCnt_1us)
 {
