f2a84a88cb8623bed14e80ea20b1fa7a057d18a2
ming6842 <ming6842@hotmail.com>
2014-11-24 16:14:32 +0800

    PWM is running

--- stm32f429_TIM_PWM/main.c
+++ stm32f429_TIM_PWM/main.c
@@ -76,6 +76,57 @@ void Timer_Initialization(void)
 }
 
 
+void enable_tim1(void)
+{
+
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_TIM1, ENABLE);
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOE , ENABLE); 
+
+  /* -- GPIO Configuration ---------------------------------------------------- */
+  GPIO_PinAFConfig(GPIOE, GPIO_PinSource11, GPIO_AF_TIM1);
+
+  GPIO_InitTypeDef GPIO_InitStruct;
+  GPIO_InitStruct.GPIO_Pin =  GPIO_Pin_11 ;
+  GPIO_InitStruct.GPIO_Mode = GPIO_Mode_AF;
+  GPIO_InitStruct.GPIO_Speed = GPIO_Speed_100MHz;
+  GPIO_InitStruct.GPIO_OType = GPIO_OType_PP;
+  GPIO_InitStruct.GPIO_PuPd = GPIO_PuPd_UP;
+
+  GPIO_Init(GPIOE, &GPIO_InitStruct);
+
+  /* -- Timer Configuration --------------------------------------------------- */
+  TIM_DeInit(TIM1);
+
+  TIM_TimeBaseInitTypeDef TIM_TimeBaseStruct;
+  TIM_TimeBaseStruct.TIM_Period = (uint32_t)(25000 - 1);  //2.5ms , 400Hz
+  TIM_TimeBaseStruct.TIM_Prescaler = (uint16_t)(180 - 1); //84 = 1M(1us)
+  TIM_TimeBaseStruct.TIM_ClockDivision = TIM_CKD_DIV1;
+  TIM_TimeBaseStruct.TIM_RepetitionCounter = 0;
+  TIM_TimeBaseStruct.TIM_CounterMode = TIM_CounterMode_Up;
+
+  TIM_TimeBaseInit(TIM1, &TIM_TimeBaseStruct);
+
+
+  /*TIM2 TIM3 TIM4 TIM8 */
+  TIM_OCInitTypeDef TIM_OCInitStruct;
+  TIM_OCInitStruct.TIM_OCMode = TIM_OCMode_PWM1;
+  TIM_OCInitStruct.TIM_OutputState = TIM_OutputState_Enable;
+  TIM_OCInitStruct.TIM_Pulse = 1000-1;
+  TIM_OCInitStruct.TIM_OCPolarity = TIM_OCPolarity_High;
+  TIM_OCInitStruct.TIM_OCNPolarity = TIM_OCNPolarity_High;
+  TIM_OCInitStruct.TIM_OCIdleState = TIM_OCIdleState_Reset;
+  TIM_OCInitStruct.TIM_OCNIdleState = TIM_OCIdleState_Reset;
+
+  TIM_OC2Init(TIM1, &TIM_OCInitStruct);
+
+  TIM_OC2PreloadConfig(TIM1, TIM_OCPreload_Enable);
+
+  TIM_ARRPreloadConfig(TIM1, ENABLE);
+  TIM_Cmd(TIM1, ENABLE);
+  TIM_CtrlPWMOutputs(TIM1, ENABLE);
+}
+
+
 void LED3_Toggle(void){
 
 
@@ -89,7 +140,10 @@ int main(void)
     RCC_Configuration();
     GPIO_Configuration();
     LED_Initialization();
-    Timer_Initialization();
+    //Timer_Initialization();
+    enable_tim1();
+
+    //TIM1->CCR2 = 1000;
     while(1)
     {
         //LED3_Toggle();
