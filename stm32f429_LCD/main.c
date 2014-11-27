9fb5af293faca0244aea6a02896073378b51645d
ming6842 <ming6842@hotmail.com>
2014-11-27 18:19:12 +0800

    Clean up and add prototype

--- stm32f429_LCD/main.c
+++ stm32f429_LCD/main.c
@@ -1,49 +1,5 @@
-/**
-  ******************************************************************************
-  * @file    Touch_Panel/main.c
-  * @author  MCD Application Team
-  * @version V1.0.1
-  * @date    11-November-2013
-  * @brief   This example describes how to configure and use the touch panel 
-  *          mounted on STM32F429I-DISCO boards.
-  ******************************************************************************
-  * @attention
-  *
-  * <h2><center>&copy; COPYRIGHT 2013 STMicroelectronics</center></h2>
-  *
-  * Licensed under MCD-ST Liberty SW License Agreement V2, (the "License");
-  * You may not use this file except in compliance with the License.
-  * You may obtain a copy of the License at:
-  *
-  *        http://www.st.com/software_license_agreement_liberty_v2
-  *
-  * Unless required by applicable law or agreed to in writing, software 
-  * distributed under the License is distributed on an "AS IS" BASIS, 
-  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  * See the License for the specific language governing permissions and
-  * limitations under the License.
-  *
-  ******************************************************************************
-  */
-
-/* Includes ------------------------------------------------------------------*/
 #include "main.h"
 
-/** @addtogroup STM32F429I_DISCOVERY_Examples
-  * @{
-  */
-
-/** @addtogroup Touch_Panel
-  * @{
-  */
-
-/* Private functions ---------------------------------------------------------*/
-
-/**
-  * @brief   Main program
-  * @param  None
-  * @retval None
-  */
 static inline void Delay_1us(uint32_t nCnt_1us)
 {
   volatile uint32_t nCnt;
@@ -68,14 +24,7 @@ int main(void)
   uint8_t colorR =0 ,colorG =0 ,colorB =0 ;
   uint8_t colorR_dir =0 ,colorG_dir =0 ,colorB_dir =0 ;
   char lcd_text_buff[100];
-  uint32_t test_int32 =0;
-  float test_float =1.222f;
-  /*!< At this stage the microcontroller clock setting is already configured, 
-  this is done through SystemInit() function which is called from startup
-  file (startup_stm32f429_439xx.s) before to branch to application main.
-  To reconfigure the default setting of SystemInit() function, refer to
-  system_stm32f4xx.c file
-  */      
+
 
   /* LCD initialization */
   LCD_Init();
@@ -93,8 +42,6 @@ int main(void)
   LTDC_Cmd(ENABLE);
   
   /* Set LCD foreground layer */
-  
-
 
   /* Clear the LCD */ 
   LCD_Clear(LCD_COLOR_WHITE);
@@ -105,33 +52,16 @@ int main(void)
 
   LCD_DisplayStringLine(LINE(1), (uint8_t*)" LCD text print example ");
   LCD_DisplayStringLine(LINE(2), (uint8_t*)" Ming6842 @ github");
-  LCD_DisplayStringLine(LINE(3), (uint8_t*)" -------------------");
-  LCD_DisplayStringLine(LINE(5), (uint8_t*)" !@#$%%^&*()_+)(*&^%%$#$%%^&*");
-  LCD_DisplayStringLine(LINE(6), (uint8_t*)" !@#$%%^&*()_+)(*&^%%$#$%%^&*");
-  LCD_DisplayStringLine(LINE(7), (uint8_t*)" !@#$%%^&*()_+)(*&^%%$#$%%^&*");
-  LCD_DisplayStringLine(LINE(8), (uint8_t*)" !@#$%%^&*()_+)(*&^%%$#$%%^&*");
-  LCD_DisplayStringLine(LINE(9), (uint8_t*)" !@#$%%^&*()_+)(*&^%%$#$%%^&*");
-  LCD_DisplayStringLine(LINE(10), (uint8_t*)" !@#$%%^&*()_+)(*&^%%$#$%%^&*");
-  LCD_DisplayStringLine(LINE(11), (uint8_t*)" !@#$%%^&*()_+)(*&^%%$#$%%^&*");
-  LCD_DisplayStringLine(LINE(12), (uint8_t*)" !@#$%%^&*()_+)(*&^%%$#$%%^&*");
 
-
-    //LCD_SetColors(ASSEMBLE_RGB(colorR, colorG, colorB),LCD_COLOR_BLACK);
-    //LCD_DrawFullRect(0,0,240,320);
     LCD_SetLayer(LCD_FOREGROUND_LAYER);
-
 
     LCD_SetColors(LCD_COLOR_WHITE,LCD_COLOR_WHITE);
     LCD_DrawFullRect(0,0,240,320);
 
-    //LCD_SetTransparency(122);
     LCD_Clear(LCD_COLOR_WHITE);
 
 #define X_MIDDLE 120
 #define Y_MIDDLE 180
-
-
-
 
     LCD_SetTextColor(LCD_COLOR_BLACK);
     LCD_DrawUniLine(X_MIDDLE+75, Y_MIDDLE-75  , X_MIDDLE-75, Y_MIDDLE+75);
@@ -163,14 +93,9 @@ int main(void)
 
     LCD_DisplayStringLine(LINE(1), (uint8_t*)"     Ce-91     ");
 
-    Delay_1us(10000);
-
-    // LCD_SetColors(LCD_COLOR_WHITE,LCD_COLOR_WHITE);
-    // LCD_DrawFullRect(0,0,240,320);
   LCD_SetLayer(LCD_BACKGROUND_LAYER);
   while (1)
   {
-
 
     if(colorR_dir){
 
@@ -211,68 +136,12 @@ int main(void)
       if(colorB<25) colorB_dir=1;
     }
 
-
     LCD_SetColors(ASSEMBLE_RGB(colorR, colorG, colorB),LCD_COLOR_BLACK);
     LCD_DrawFullRect(0,0,240,320);
 
-
-
-
-    // LCD_SetTextColor(LCD_COLOR_WHITE);
-    // LCD_SetLayer(LCD_FOREGROUND_LAYER);
-    // //LCD_DrawFullCircle(120, 240, 40);
-    // Delay_1us(10000);
-
-    // LCD_SetColors(ASSEMBLE_RGB(colorR, colorG, colorB),LCD_COLOR_BLACK);
-    // //sLCD_DrawFullCircle(120, 240, 30);
-
-
-    Delay_1us(10000);
-
-     // LCD_Clear(ASSEMBLE_RGB(colorR, colorG, colorB));
-
-
-      //sprintf(lcd_text_buff," LOLOLOL %ld",test_int32);
-      //LCD_DisplayStringLine(LINE(7), (uint8_t*)lcd_text_buff);
-
-      //LCD_DisplayStringLine(LINE(8), (uint8_t*)lcd_text_buff);
+    Delay_1us(30000);
 
   }
   
 }
 
-/**
-* @brief  Configure the IO Expander and the Touch Panel.
-* @param  None
-* @retval None
-*/
-#ifdef  USE_FULL_ASSERT
-
-/**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
-void assert_failed(uint8_t* file, uint32_t line)
-{
-  /* User can add his own implementation to report the file name and line number,
-     ex: printf("Wrong parameters value: file %s on line %d\r\n", file, line) */
-
-  /* Infinite loop */
-  while (1)
-  {
-  }
-}
-#endif
-
-/**
-  * @}
-  */ 
-
-/**
-  * @}
-  */ 
-
-/************************ (C) COPYRIGHT STMicroelectronics *****END OF FILE****/
