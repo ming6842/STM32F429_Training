626e6886cf8b927f59891827d7d31701777070fd
ming6842 <ming6842@hotmail.com>
2015-07-29 12:29:01 +0800

    ADC is reading

--- stm32f429_thrust_test_stand/main.c
+++ stm32f429_thrust_test_stand/main.c
@@ -9,29 +9,78 @@ static inline void Delay_1us(uint32_t nCnt_1us)
 }
 
 
+void ADC_Initialization(void)
+{
+  ADC_InitTypeDef       ADC_InitStructure;
+  ADC_CommonInitTypeDef ADC_CommonInitStructure;
+  GPIO_InitTypeDef      GPIO_InitStructure;
+
+  /* Enable ADC1, DMA2 and GPIO clocks ****************************************/
+  RCC_AHB1PeriphClockCmd(RCC_AHB1Periph_GPIOA, ENABLE);
+  RCC_APB2PeriphClockCmd(RCC_APB2Periph_ADC1, ENABLE);
+
+  /* Configure ADC1 Channel7 pin as analog input ******************************/
+  GPIO_InitStructure.GPIO_Pin = GPIO_Pin_5;
+  GPIO_InitStructure.GPIO_Mode = GPIO_Mode_AN;
+  GPIO_InitStructure.GPIO_PuPd = GPIO_PuPd_NOPULL ;
+  GPIO_Init(GPIOA, &GPIO_InitStructure);
+
+  /* ADC Common Init **********************************************************/
+  ADC_CommonInitStructure.ADC_Mode = ADC_Mode_Independent;                     // No external trigger is connected
+  ADC_CommonInitStructure.ADC_Prescaler = ADC_Prescaler_Div2;                  // ADC clock prescaler
+  ADC_CommonInitStructure.ADC_DMAAccessMode = ADC_DMAAccessMode_Disabled;      // No DMA (polling mode)
+  ADC_CommonInitStructure.ADC_TwoSamplingDelay = ADC_TwoSamplingDelay_5Cycles;  
+  ADC_CommonInit(&ADC_CommonInitStructure);
+
+  /* ADC1 Init ****************************************************************/
+  ADC_InitStructure.ADC_Resolution = ADC_Resolution_12b;                       // Resolution 12 bits
+  ADC_InitStructure.ADC_ScanConvMode = DISABLE;                                // Use single channel 
+  ADC_InitStructure.ADC_ContinuousConvMode = ENABLE;                           // Continue conversion
+  ADC_InitStructure.ADC_ExternalTrigConvEdge = ADC_ExternalTrigConvEdge_None;
+  ADC_InitStructure.ADC_ExternalTrigConv = ADC_ExternalTrigConv_T1_CC1;
+  ADC_InitStructure.ADC_DataAlign = ADC_DataAlign_Right;                       // Data bits shifted to right hand side (Low)
+  ADC_InitStructure.ADC_NbrOfConversion = 1;                                   // Convert only once
+  ADC_Init(ADC1, &ADC_InitStructure);
+
+  /* ADC1 regular channel7 configuration *************************************/
+  ADC_RegularChannelConfig(ADC1, ADC_Channel_5, 1, ADC_SampleTime_3Cycles);
+
+  /* Enable ADC1 */
+  ADC_Cmd(ADC1, ENABLE);
+}
+
+void LED3_Toggle(void){
+
+
+  GPIOG->ODR ^= GPIO_Pin_13;
+
+}
 int main(void)
 {
 
   char lcd_text_main[100];
   uint32_t runner=0;
-
+  uint16_t adc_data1;
+    ADC_Initialization();
     lcd_init();
     lcd_drawBackground(20,60,250);
     lcd_drawBGPersimmon(20, 60, 250);
     LCD_SetColors(LCD_COLOR_WHITE-1,LCD_COLOR_WHITE);
 
-    LCD_SetFont(&Font8x12); 
+    LCD_SetFont(&Font16x24); 
 
+    ADC_SoftwareStartConv(ADC1);
     terminalBufferInitilization();
+        Delay_1us(50);
 
   while (1){
 
-
-     sprintf(lcd_text_main,"\nThis is line : %ld ", runner++); 
+    LED3_Toggle();
+    adc_data1 = ADC_GetConversionValue(ADC1);
+     sprintf(lcd_text_main,"ADC : %d    ", adc_data1); 
 
      LCD_DisplayStringLine(LINE(2), (uint8_t*)lcd_text_main);
 
-     //Delay_1us(1000000);
   }
   
 }
